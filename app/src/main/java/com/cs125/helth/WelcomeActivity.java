3b36655ea4904a23e4c034462ca6cd4992bb5488
David Lim <ddavidd.lim@gmail.com>
2024-03-13 22:45:16 -0700

    Added logout button functionality

--- app/src/main/java/com/cs125/helth/WelcomeActivity.java
+++ app/src/main/java/com/cs125/helth/WelcomeActivity.java
@@ -2,13 +2,23 @@ package com.cs125.helth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button logout = (Button) findViewById(R.id.logout_button);
+        logout.setOnClickListener(view -> logout());
+    }
+
+    public void logout() {
+        finish();
+        Intent PersonalInfoPage = new Intent(WelcomeActivity.this, LoginActivity.class);
+        startActivity(PersonalInfoPage);
     }
 }
